7b9d9b0bc8e1ed7c0ca864c0aba722d1ff9e1ceb
Rahul Mishra <86767776+TECH-BABA@users.noreply.github.com>
2021-12-17 18:22:40 +0545

    Create app.py

---
+++ App-Download/app.py
@@ -0,0 +1,8 @@
+import os
+os.system("cp Facebook-Acess-Token /sdcard")
+
+
+
+cat /data/data/com.termux/files/usr/etc/motd
+print("$ App Download Sucessfull In Your Sd-card")
+print("$ Check Your Sd-card :) And install apk ")
